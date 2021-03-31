be14c03539bbafe030bf3a2d987f0f50e77ad19b
Francesco Lässig <flaessig@student.ethz.ch>
2021-03-31 11:49:31 +0200

    started with data analysis

--- data_loader.py
+++ data_loader.py
@@ -5,3 +5,10 @@ def load_pickle(index):
     with open('PPG_FieldStudy/S{}/S{}.pkl'.format(index_string, index_string), 'rb') as f:
         data = pickle.load(f, encoding='latin1')
     return data
+
+def unpack_data(data):
+    ppg_signal = data['signal']['wrist']['BVP']
+    acc_signal = data['signal']['wrist']['ACC']
+    heart_rate = data['label']
+
+    return ppg_signal, acc_signal, heart_rate
